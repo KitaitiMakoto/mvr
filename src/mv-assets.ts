c2fbbbd617f7ccdd81980526b0a81cffaffdf956
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-05-20 05:42:32 +0900

    Implement <mv-assets>

---
+++ src/mv-assets.ts
@@ -0,0 +1,174 @@
+import {LitElement, css, html} from 'lit';
+import {customElement, property, state} from 'lit/decorators.js';
+
+interface Images {
+  name?: string;
+  items: {
+    src: string;
+  }[];
+}
+
+interface ImagesList {
+  items: Images[];
+}
+
+type Assets = Images | {
+  items: Images[]
+};
+
+function isImagesList(assets: Assets): assets is ImagesList {
+  return 'items' in assets;
+}
+
+@customElement('mv-assets')
+export class MvAssets extends LitElement {
+  static styles = [
+    css`
+      :host {
+        display: block;
+        position: relative;
+      }
+
+      h2 {
+        position: sticky;
+        inset-block-start: 0;
+        background-color: white;
+        z-index: 12;
+      }
+
+      ul {
+        padding: 0;
+        list-style: none;
+      }
+
+      .images {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        gap: 1rem;
+        direction: var(--direction, 'rtl');
+        text-align: center;
+      }
+
+      .images li {
+        position: relative;
+      }
+
+      input[type="checkbox"] {
+        position: absolute;
+        inset-block-start: 0;
+        inset-inline-start: 0;
+        margin: 0;
+        inline-size: 1rem;
+        aspect-ratio: 1;
+        line-height: 1;
+      }
+
+      figure {
+        margin: 0;
+      }
+
+      img {
+        max-width: 100%;
+      }
+    `
+  ];
+
+  render() {
+    if (! this.open) {
+      return;
+    }
+
+    if (this._error) {
+      return html`<p>${this._error}</p>`;
+    }
+
+    if (! this.src) {
+      return html`<p>ソースが設定されていません</p>`;
+    }
+
+    if (! this.srcObject) {
+      return html`<p>Loading...</p>`;
+    }
+
+    if (isImagesList(this.srcObject)) {
+      return html`
+        <ul class='images-list'>
+          ${this.srcObject.items.map(item => html`
+            <li>
+              ${this.#renderImages(item)}
+            </li>
+          `)}
+        </ul>
+      `
+    }
+
+    return this.#renderImages(this.srcObject);
+  }
+
+  #renderImages({name, items: images}: Images) {
+    return html`
+      ${name === undefined ? undefined : html`<h2>${name}</h2>`}
+      <ul class='images'>
+        ${images.map(({src}) => html`
+          <li>
+            <input type="checkbox">
+            <figure>
+              <img src='${src}' alt=''>
+              <figcaption>${new URL(src, location.href).pathname.split('/').at(-1)}</figcaption>
+            </figure>
+          </li>
+        `)}
+      </ul>
+    `;
+  }
+
+  @property({type: Boolean, reflect: true})
+  open = false;
+
+  @property({reflect: true})
+  src = '';
+
+  @property()
+  srcObject: Assets | null = null;
+
+  @property({reflect: true})
+  direction: 'ltr' | 'rtl' = 'rtl';
+
+  @state()
+  private _error?: string;
+
+  attributeChangedCallback(name: string, old: string | null, value: string | null): void {
+    super.attributeChangedCallback?.(name, old, value);
+    switch (name) {
+      case 'src':
+        if (value) {
+          this.#load();
+        } else {
+          this.srcObject = null;
+        }
+      break;
+      case 'direction':
+        switch (value) {
+          case 'ltr':
+            this.style.setProperty('--direction', 'ltr');
+            break;
+          case 'rtl':
+            this.style.setProperty('--direction', 'rtl');
+            break;
+        }
+        break;
+    }
+  }
+
+  async #load() {
+    this._error = undefined;
+    this.srcObject = null;
+    this.srcObject = await fetch(this.src).then(res => res.json())
+      .catch(err => {
+        console.debug(err);
+        this._error = err;
+        this.dispatchEvent(new ErrorEvent(err));
+      });
+    this.dispatchEvent(new Event('load'));
+  }
+}
